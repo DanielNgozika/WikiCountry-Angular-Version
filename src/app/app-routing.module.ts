2072a7a049470eb51fb84e9cfd8ed17a8fbc0445
Daniel Ngozika <danielngozikac@gmail.com>
2020-12-03 19:58:09 +0100

    Configure routes.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SearchResultComponent } from "./components/search-result/search-result.component"
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "search_result", component: SearchResultComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
