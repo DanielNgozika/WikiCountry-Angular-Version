74ab45d17e6a85964f573b7d66c85a5e51259bd9
Daniel Ngozika <danielngozikac@gmail.com>
2020-12-03 19:57:05 +0100

    Move home functionalities from app component to home component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Angular-Project';
 
-  search = ():void => {
-    alert("working")
-  }
+  title = 'Angular-Project';
 }
