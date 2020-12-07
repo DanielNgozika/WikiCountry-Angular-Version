d338cec4655b5f64a21f538f91d7f0edabc3354c
Daniel Ngozika <danielngozikac@gmail.com>
2020-12-07 14:58:03 +0100

    Create functionality for fetching country detail.

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CountryServiceService } from "../../services/country-service.service"
 
 @Component({
   selector: 'app-search-result',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchResultComponent implements OnInit {
 
-  constructor() { }
+  result: any = null;
+  
+  constructor(private countryService: CountryServiceService ) { }
+  
+  country: any = localStorage.getItem("country keyword")
 
   ngOnInit(): void {
+    this.getCountry();
+  }
+
+  getCountry() {
+    this.countryService.getCountry(localStorage.getItem("country keyword")).subscribe(country => { this.result = country[0] });
+    localStorage.setItem("loading", "false");
+    this.country = localStorage.getItem("country keyword");
+  }
+
+  isLoading() {
+    const loading = localStorage.loading;
+    console.log(loading === "true" ? true : false)
+    return loading === "true" ? true : false;
+  }
+
+  search = ():void => {
+    this.getCountry();
   }
 
 }
