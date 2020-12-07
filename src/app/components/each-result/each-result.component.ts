06ae154a678f4f68af8655e4346b3c006fb7419f
Daniel Ngozika <danielngozikac@gmail.com>
2020-12-07 15:14:54 +0100

    Add test case for Input.

--- src/app/components/each-result/each-result.component.ts
+++ src/app/components/each-result/each-result.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-each-result',
@@ -11,5 +11,7 @@ export class EachResultComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() result: any = null;
 
 }
